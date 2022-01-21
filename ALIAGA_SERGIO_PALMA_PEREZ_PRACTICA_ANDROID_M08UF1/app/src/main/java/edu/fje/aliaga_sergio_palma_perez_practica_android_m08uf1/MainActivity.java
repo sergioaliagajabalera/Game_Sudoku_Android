6894e89f80cffb0ef1a8350190f00c0a9864f5b1
Sergio Aliaga Jabalera <74768958+sergioaliagajabalera@users.noreply.github.com>
2022-01-21 17:25:02 +0100

    firtscommit

---
+++ ALIAGA_SERGIO_PALMA_PEREZ_PRACTICA_ANDROID_M08UF1/app/src/main/java/edu/fje/aliaga_sergio_palma_perez_practica_android_m08uf1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fje.aliaga_sergio_palma_perez_practica_android_m08uf1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
